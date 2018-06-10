f181eec9acc248b096a5ace714107dc3fc220ac6
forlo581230 <forlo581230@yahoo.com.tw>
2018-06-10 17:33:07 +0800

    setInterval

--- WebServer/webServer.js
+++ WebServer/webServer.js
@@ -1,11 +1,28 @@
-var express = require('express')
+var express = require('express');
+var bodyParser = require('body-parser');
+var http = require('http');
+var path = require('path');
 var app = express();
+
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(__dirname+"/dist"));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
-app.listen(4200);
+
+var port = process.env.PORT || '4200';
+app.set('port', port);
+var server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`API running on localhost:${port}`));
+
+// app.listen(4200);
 // app.use(function (req, res, next) {
 
 //     res.header("Access-Control-Allow-Origin", "*");
