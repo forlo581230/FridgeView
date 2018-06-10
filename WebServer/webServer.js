cb966c84c23ae6455691f8e719ed6d1ad32dd382
forlo581230 <forlo581230@yahoo.com.tw>
2018-06-10 17:06:58 +0800

    webServer

--- WebServer/webServer.js
+++ WebServer/webServer.js
@@ -2,12 +2,15 @@ var express = require('express')
 var app = express();
 
 app.use(express.static(__dirname+"/dist"));
-app.listen(4200);
-app.use(function (req, res, next) {
-
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    // Pass to next layer of middleware
-    next();
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
+app.listen(4200);
+// app.use(function (req, res, next) {
+
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     // Pass to next layer of middleware
+//     next();
+// });
