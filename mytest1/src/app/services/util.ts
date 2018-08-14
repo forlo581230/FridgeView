c0150e81e8a7aac1c8dfbc4f2dad5f7a935f5962
forlo581230 <forlo581230@yahoo.com.tw>
2018-08-14 14:32:47 +0800

    fix

--- mytest1/src/app/services/util.ts
+++ mytest1/src/app/services/util.ts
@@ -1,6 +1,5 @@
 export class Util {
-    // serverUrl:string='http://140.124.182.45';//'http://192.168.126.137';
-    serverUrl:string='localhost';
+    serverUrl:string='http://10.105.1.219';//'http://192.168.126.137';
     constructor(){
     }
 }
