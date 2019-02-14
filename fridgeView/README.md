a0df08502ab7d76dc0a8a26818dcb2b7ec7d0aa0
AlexDZ <forlo581230@yahoo.com.tw>
2019-02-14 15:41:45 +0800

    fix fridgeView

--- fridgeView/README.md
+++ fridgeView/README.md
@@ -1,4 +1,4 @@
-# Mytest
+# firdgeView (for webServer2)
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.6.0.
 
--- fridgeView/src/app/dashboard/dashboard.component.ts
+++ fridgeView/src/app/dashboard/dashboard.component.ts
@@ -19,17 +19,18 @@ export const READER_MAC: string[] = ['', 'rfid_1', 'rfid_2', 'rfid_3', 'rfid_4']
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
+  id: number;
   title: string;
   date: string;
   counter_time: number;
   counter_index: number;
+  durationOfEvery3Job: number;
+  totalTimeOfCurrentJob: number;
   wip: number = -1;
 
   // @ViewChild(PieComponent) pie: PieComponent;
   @ViewChild(BarComponent) bar: BarComponent;
   @ViewChild(WipDashboardComponent) WipDashboard: WipDashboardComponent;
-  id: number;
 
   currentFridges: Fridge[] = [];  //current jobNumbers
   output: Fridge[] = [];          //current outputs
@@ -45,13 +46,11 @@ export class DashboardComponent implements OnInit {
 
   second: number = 1; //for interval
   position_1() {
-    this.counter_time = 0;
-    let durationOfEvery3Job: number = 30 / (this.currentFridges.length / 3);
-    let totalTimeOfCurrentJob = durationOfEvery3Job;
     var loop = setInterval(() => {
-      if ((this.counter_time > totalTimeOfCurrentJob) &&
+      if ((this.counter_time > this.totalTimeOfCurrentJob) &&
         (this.counter_index < this.currentFridges.length)) {
-        totalTimeOfCurrentJob += durationOfEvery3Job
+        console.log('--switch page--');
+        this.totalTimeOfCurrentJob += this.durationOfEvery3Job
         this.counter_index += 3;
         this.showcurrentFridges = this.currentFridges.slice(this.counter_index, this.counter_index + 3);
         this.showtables = this.tables.slice(this.counter_index, this.counter_index + 3);
@@ -61,8 +60,7 @@ export class DashboardComponent implements OnInit {
       }
 
       if (this.counter_time == 31) {
-        this.counter_time = 0;
-        this.counter_index = 0;
+        this.updateData();
       }
       // switch (this.counter_time) {
       //   case 15:
@@ -106,13 +104,11 @@ export class DashboardComponent implements OnInit {
     }, 1000 * this.second);
   }
   position_2() {
-    this.counter_time = 0;
-    let durationOfEvery3Job: number = 30 / (this.currentFridges.length / 3);
-    let totalTimeOfCurrentJob = durationOfEvery3Job;
     var loop = setInterval(() => {
-      if ((this.counter_time > totalTimeOfCurrentJob) &&
+      if ((this.counter_time > this.totalTimeOfCurrentJob) &&
         (this.counter_index < this.currentFridges.length)) {
-        totalTimeOfCurrentJob += durationOfEvery3Job
+        console.log('--switch page--');
+        this.totalTimeOfCurrentJob += this.durationOfEvery3Job
         this.counter_index += 3;
         this.showcurrentFridges = this.currentFridges.slice(this.counter_index, this.counter_index + 3);
         this.showtables = this.tables.slice(this.counter_index, this.counter_index + 3);
@@ -122,8 +118,8 @@ export class DashboardComponent implements OnInit {
       }
 
       if (this.counter_time == 31) {
-        this.counter_time = 0;
-        this.counter_index = 0;
+        this.updateData();
+        this.totalTimeOfCurrentJob = this.durationOfEvery3Job
       }
       // switch (this.counter_time) {
       //   case 15:
@@ -166,13 +162,11 @@ export class DashboardComponent implements OnInit {
     }, 1000 * this.second);
   }
   position_3() {
-    this.counter_time = 0;
-    let durationOfEvery3Job: number = 30 / (this.currentFridges.length / 3);
-    let totalTimeOfCurrentJob = durationOfEvery3Job;
     var loop = setInterval(() => {
-      if ((this.counter_time > totalTimeOfCurrentJob) &&
+      if ((this.counter_time > this.totalTimeOfCurrentJob) &&
         (this.counter_index < this.currentFridges.length)) {
-        totalTimeOfCurrentJob += durationOfEvery3Job
+        console.log('--switch page--');
+        this.totalTimeOfCurrentJob += this.durationOfEvery3Job
         this.counter_index += 3;
         this.showcurrentFridges = this.currentFridges.slice(this.counter_index, this.counter_index + 3);
         this.showtables = this.tables.slice(this.counter_index, this.counter_index + 3);
@@ -182,8 +176,8 @@ export class DashboardComponent implements OnInit {
       }
 
       if (this.counter_time == 31) {
-        this.counter_time = 0;
-        this.counter_index = 0;
+        this.updateData();
+        this.totalTimeOfCurrentJob = this.durationOfEvery3Job
       }
       // switch (this.counter_time) {
       //   case 15:
@@ -224,15 +218,16 @@ export class DashboardComponent implements OnInit {
   }
 
   ngOnInit() {
-    const id = +this.route.snapshot.paramMap.get('id');
-    this.title = RFID[id];
+    this.id = +this.route.snapshot.paramMap.get('id');
+    this.title = RFID[this.id];
     this.date = moment().format('日期: YYYY-MM-DD 時間: HH時mm分');
-    this.counter_time = 0;
+
+    this.counter_time = 31;
     this.counter_index = 0;
+  }
 
-    this.getJobNumbers(READER_MAC[id]);
-
-    switch (id) {
+  ngAfterViewInit() {
+    switch (this.id) {
       case 1:
         this.position_1();
         break;
@@ -243,12 +238,24 @@ export class DashboardComponent implements OnInit {
         this.position_3();
         break;
     }
-
-    // Bar chart
   }
-  // ngAfterViewInit() {
-  //   this.pie.drawPie(39 / 100, 0, 0, 0);
-  // }
+
+  async updateData() {
+    this.counter_time = 0;
+    this.counter_index = 0;
+    this.tables = [];
+    this.currentFridges = [];
+    this.completionRate = [];
+    this.jobNumber = [];
+    this.totalAmount = 0;
+    console.log('--getNewData--');
+    await this.getJobNumbers(READER_MAC[this.id]);
+    // await this.getJobNumbers_test(READER_MAC[this.id]);
+    //1份工單有3個，將顯示時間均分
+    this.durationOfEvery3Job = 30 / Math.ceil(this.currentFridges.length / 3);
+    this.totalTimeOfCurrentJob = this.durationOfEvery3Job;
+    console.log(this.durationOfEvery3Job);
+  }
 
   completionRate: number[];
   jobNumber: string[];
@@ -264,23 +271,21 @@ export class DashboardComponent implements OnInit {
         this.completionRate = [];
         this.jobNumber = [];
 
-        console.log('目前工號數量:');
-        console.log(elements);
         let numJob = elements.length;
         this.currentFridges = elements;
+        console.log('目前工號數量:'+numJob);
 
-        // if (numJob <= 3) {
         //compute output
+        // 產量時間計算
         for (let i = 0; i < numJob; i++) {
           await this.getSection(this.currentFridges[i]);
           console.log(this.currentFridges[i].job_number + '_五個時段 : ');
           console.log(this.output);
-
           let table: Table = Table.CreateDefault();
           let acOuput = 0;
           let target = parseInt(this.currentFridges[i].target.toString());
           let maximumAmount = 0;
-          //this.output.length==4
+
           for (let j = 0; j < this.output.length; j++) {
             if (this.output[j]) {
               //只使用到 amount
@@ -316,15 +321,11 @@ export class DashboardComponent implements OnInit {
           console.log(table);
           this.tables.push(table);
           this.totalAmount += maximumAmount;
-
           this.totalTarge += parseInt(this.currentFridges[i].target.toString());
-
           this.completionRate.push(parseInt((maximumAmount / parseInt(this.currentFridges[i].target.toString()) * 100).toFixed()));
           this.jobNumber.push(this.currentFridges[i].job_number.toString());
-          // this.jobNumber.push('工號 ('+(i+1).toString()+')');
         }
-        console.log('num:' + numJob);
-        // for (let i = 2; i < numJob%4%3+4; i++) {
+        //補滿格子
         for (let i = 0; i < (numJob % 3 - 3) * -1; i++) {
           this.currentFridges.push(Fridge.CreateDefault());
           this.tables.push(Table.CreateEmpty());
@@ -333,6 +334,7 @@ export class DashboardComponent implements OnInit {
           this.completionRate = [100, 100, 100];
           this.jobNumber = ['?', '?', '?'];
         }
+        console.log('結果');
         console.log(this.tables);
 
         //一次取三份
@@ -340,20 +342,60 @@ export class DashboardComponent implements OnInit {
         this.showcurrentFridges = this.currentFridges.slice(this.counter_index, this.counter_index + 3);
         this.showjobNumber = this.jobNumber.slice(this.counter_index, this.counter_index + 3);
         this.showcompletionRate = this.completionRate.slice(this.counter_index, this.counter_index + 3);
-        // }
+
         //更新圖表
         // this.pie.drawPie(this.totalAmount / this.totalTarge, 0, 0, 0, this.totalAmount.toString());
         this.bar.init(this.showcompletionRate, this.showjobNumber, this.totalAmount);
+
+        return new Promise((resolve, reject) => {
+          try {
+            resolve([]);
+          }
+          catch (error) {
+            reject(error);
+          }
+        })
       }
     )
   }
+
+  getJobNumbers_test(reader_mac: string) {
+    const numJob = 0;
+    // 用四筆工單測試
+    for (let i = 0; i < 4; i++) {
+      this.currentFridges.push(Fridge.CreateDefault());
+      this.tables.push(Table.CreateEmpty());
+    }
+    if (this.completionRate.length == 0) {
+      this.completionRate = [100, 100, 100];
+      this.jobNumber = ['?', '?', '?'];
+    }
+    console.log('結果');
+    console.log(this.tables);
+    //一次取三份
+    this.showtables = this.tables.slice(this.counter_index, this.counter_index + 3);
+    this.showcurrentFridges = this.currentFridges.slice(this.counter_index, this.counter_index + 3);
+    this.showjobNumber = this.jobNumber.slice(this.counter_index, this.counter_index + 3);
+    this.showcompletionRate = this.completionRate.slice(this.counter_index, this.counter_index + 3);
+    //更新圖表
+    this.bar.init(this.showcompletionRate, this.showjobNumber, this.totalAmount);
+    return new Promise((resolve, reject) => {
+      try {
+        resolve([]);
+      }
+      catch (error) {
+        reject(error);
+      }
+    })
+
+  }
   //get all time section
+  //07:50~10:00 	10:10~11:50 	12:40~15:00 	15:10~16:40 	加班
   getSection(tmpFridge: Fridge) {
     let currentTime = moment().format('YYYY-MM-DD');
     let currenTarge = 0;
     return new Promise((resolve, reject) => {
       try {
-        // let obj :Fridge[]= [];
         this.appService.getFridges(currentTime, tmpFridge.reader_mac, tmpFridge.job_number).subscribe(
           fridges => {
             this.output = (fridges);
